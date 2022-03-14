fc7550010870d70312cef25f6230bbb3f199acd2
thequinneffect <n_quinny12@hotmail.com>
2022-03-14 22:11:43 +1100

    Lecture 7 - Animating UMG Widgets
    * Created a widget animation, PulseHealthTextAnim.
      * Lasts 0.5 seconds, lerps the text to 1.5 scale over the first 0.25 seconds, then returns to normal scale over the last 0.25 seconds.
    * Also added a minimum width of 75 so that when the number changes from 3 chars (e.g. 100, -20) to 2 chars (e.g. 80), it still takes up the same amount of space and doesn't change the length of the progress bar
    * Also added some padding to the text (8 on the left)

--- Source/ActionRoguelike/Public/SAttributeComponent.h
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -30,7 +30,15 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes")
 	float Health;
 
-	/* This is an actual instance of the FOnHealthChanged delegate/event/broadcaster. */
+	/* This is an actual instance of the FOnHealthChanged delegate/event/broadcaster
+	 * to which we can bind to via blueprints. Even though this is in the protected
+	 * section, because it is BlueprintAssignable, we can actually assign to this
+	 * delegate from outside the class. For example, we got a reference to the 
+	 * player pawn in the health widget, then got a reference to this component
+	 * via the pawn reference, then called the Assign On Health Change event
+	 * node to assign a new red blueprint event node to this delegate. Note that
+	 * this is an instance of the delegate, and other instances calling broadcast
+	 * wont cause this one's subscribers to be notified - they are separate. */
 	UPROPERTY(BlueprintAssignable)
 	FOnHealthChanged OnHealthChanged;
 
